ca19ae4049658302795af25767c479212cb51492
Fredrik Järnbröst <contact@fredrikvestin.com>
2018-09-24 17:07:58 +0200

    Broke out product type additional price

--- src/price-calculator.js
+++ src/price-calculator.js
@@ -3,8 +3,16 @@ var USER_TYPE_COMPANY = 1;
 var PRODUCT_TYPE_NEW = 0;
 var PRODUCT_TYPE_OLD = 1;
 
+var productTypeAdditionalPrices = {
+	[PRODUCT_TYPE_NEW]: 25,
+	[PRODUCT_TYPE_OLD]: 35,
+}
+
 // price, the price of the product
 var calculatePrice = function (userType, productType, price, publishedDate) {
+
+	var productTypeAdditionalPrice = productTypeAdditionalPrices[productType];
+
 	try	{
 		switch (userType) {
 		case USER_TYPE_NORMAL:
@@ -12,20 +20,20 @@ var calculatePrice = function (userType, productType, price, publishedDate) {
 				var enddateDiscount = 0;
 				if (publishedDate.toDateString() == new Date().toDateString()) enddateDiscount = 10;
 
-				return price + 25 - enddateDiscount;
+				return price + productTypeAdditionalPrice - enddateDiscount;
 			} else if (productType == PRODUCT_TYPE_OLD) {
-				return price + 35 - 0;
+				return price + productTypeAdditionalPrice - 0;
 			}
 			break;
 		case USER_TYPE_COMPANY:
 			if (productType == PRODUCT_TYPE_NEW) {
 				if (publishedDate.toDateString() === new Date().toDateString()) {
-						return price + 25 - 15;// Enddate discount and company discount
+						return price + productTypeAdditionalPrice - 15;// Enddate discount and company discount
 				}
 
-				return price + 25 - 5;// Only company discount
+				return price + productTypeAdditionalPrice - 5;// Only company discount
 			} else if (productType == PRODUCT_TYPE_OLD) {
-				return price + 35 - 5;
+				return price + productTypeAdditionalPrice - 5;
 			}
 			break;
 		}
