4fc46edbb70c0f0d70f9dd6d4614add0c1555e2b
Fredrik Järnbröst <contact@fredrikvestin.com>
2018-09-24 16:45:20 +0200

    Added constants for user types and product types

--- src/price-calculator.js
+++ src/price-calculator.js
@@ -1,27 +1,30 @@
-// userType, 0 = normal, 1 = company
-// productType, 0 = new product, 1 = old product
+var USER_TYPE_NORMAL = 0;
+var USER_TYPE_COMPANY = 1;
+var PRODUCT_TYPE_NEW = 0;
+var PRODUCT_TYPE_OLD = 1;
+
 // price, the price of the product
 var calculatePrice = function (userType, productType, price, publishedDate) {
 	try	{
 		switch (userType) {
-		case 0: // normal
-			if (productType == 0) { // new product
+		case USER_TYPE_NORMAL:
+			if (productType == PRODUCT_TYPE_NEW) {
 				var enddateDiscount = 0;
 				if (publishedDate.toDateString() == new Date().toDateString()) enddateDiscount = 10;
 
 				return price + 25 - enddateDiscount;
-			} else if (productType == 1) { // old product
+			} else if (productType == PRODUCT_TYPE_OLD) {
 				return price + 35 - 0;
 			}
 			break;
-		case 1: // company
-			if (productType == 0) { // new product
+		case USER_TYPE_COMPANY:
+			if (productType == PRODUCT_TYPE_NEW) {
 				if (publishedDate.toDateString() === new Date().toDateString()) {
 						return price + 25 - 15;// Enddate discount and company discount
 				}
 
 				return price + 25 - 5;// Only company discount
-			} else if (productType == 1) { // old product
+			} else if (productType == PRODUCT_TYPE_OLD) {
 				return price + 35 - 5;
 			}
 			break;
